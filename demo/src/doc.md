d32f3c0544e1d48bcb98675909957b88896a856a
bowiego <bobosndr@163.com>
2018-09-07 13:53:30 +0800

    remove unnecessary files

--- demo/src/doc.md
+++
@@ -1,6 +0,0 @@
-``` javascript
-<notice-bar
-  :text="text"
-  :bg-color="'#fff'"
-  :color="'#03a9f4'"/>
-```
--- demo/src/md2html.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs'),
-  markdown = require('markdown'),
-  fileContent
-
-fileContent = fs.readFileSync('code-example.md', 'utf8')
-fileContent = markdown.toHTML(fileContent)
-
-fs.writeFileSync('code-example.html', fileContent)
